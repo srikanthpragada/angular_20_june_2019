4fd17a35d07da252bf1ec885f592feadbb75a8e3
srikanthpragada <srikanthpragada@gmail.com>
2019-06-24 09:04:34 +0530

    24-JUNE

---
+++ app/app.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HelloComponent } from './hello.component';
+
+@NgModule({
+  declarations: [
+       HelloComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [HelloComponent]
+})
+export class AppModule { }
---
+++ app/hello.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'st-hello',
+  templateUrl: './hello.component.html'
+})
+export class HelloComponent {
+   message = 'Hello Angular';
+}
